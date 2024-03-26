891b33ec48e5894a63d9d158ea69b1fa3c8dec8f
Sujeeth Chandra Yerra <yerrasujeethchandra@gmail.com>
2024-03-26 21:36:56 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import { string } from "zod";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -7,7 +9,8 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+   DATABASE_URL:"postgresql://neondb_owner:Joj3eQT4WONR@ep-spring-glitter-a1la7l56.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+,  
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
